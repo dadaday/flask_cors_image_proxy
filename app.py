442b64d25f0833583d660ef1c7ddea6581a62ae2
dadaday <aaurub@gmail.com>
2021-05-20 01:22:39 +0600

    some improvements

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def insta_proxy():
     insta_url = request.query_string.decode('utf-8').replace('url=', '')
-    response = requests.get(insta_url, stream=True)
-    response.headers.pop('cross-origin-resource-policy')
+    if insta_url is None or insta_url == '':
+        return 'Media url is not provided', 400
+    try:
+        response = requests.get(insta_url, stream=True)
+    except:
+        return 'Wrong media url is provided', 400
+    response.headers.pop('cross-origin-resource-policy', None)
     return response.content, response.status_code, response.headers.items()
 
 
